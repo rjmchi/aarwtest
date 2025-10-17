0e60c4ca3f437a89ea3c0dfed3bbf12722eace22
rjmchi <robert@rjmprogramming.com>
2025-10-17 11:55:16 -0600

    adds deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x deploy.sh` to ensure it does
+
+#install composer dependendcies without dev dependencies
+composer install --optimize-autoloader --no-dev
+#install NPM packages
+npm i
+# Build assets using NPM
+npm run build
+# Clear cache
+php artisan optimize:clear
+# Cache the various components of the Laravel application
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+# Run any database migrations
+php artisan migrate --force
+
